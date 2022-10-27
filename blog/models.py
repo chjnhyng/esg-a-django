52e47775a6582affedf6c20f94a678ab2536e8f4
chjnhyng <slimmark@naver.com>
2022-10-27 12:00:11 +0900

    oct 27

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,11 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def get_absolute_url(self):
+        #todo 향후에는 장고의 url reverse 기능을 사용하기
+        return f"/blog/{self.pk}/"
+
 
     def __str__(self):
         return f"[{self.pk}] {self.title}"
